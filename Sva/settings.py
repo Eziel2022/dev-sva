b2b57358006c3bcd67964a9589bd7e12a76cfbb0
And1-collab <dimarco348@gmail.com>
2024-05-06 01:22:46 -0300

    index.html actualizado, creacion urls.py en app

--- Sva/settings.py
+++ Sva/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-a14n!)6qny&ak59$b65sn_+ov^62xu&!6l_h6(8o-)^l#-xzew
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
